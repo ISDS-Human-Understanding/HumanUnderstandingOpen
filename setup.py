c96cc8ab3593f599f324043e3430b4faf66b9627
Namo Bang <48271454+Namo-Bang@users.noreply.github.com>
2022-05-06 14:55:59 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='HumanUnderstanding',
+    version='1.0',
+    packages=[
+        'numpy==1.22.3',
+        'pandas==1.4.2'
+        'torch==1.11.0+cu113',
+        'torchaudio==0.11.0+cu113'
+        'scikit-learn',
+        'transformers==4.18.0',
+        'tokenizers==0.12.1'
+        'soundfile==0.10.3.post1'
+    ],
+    url='',
+    license='',
+    author='Namo-Bang',
+    author_email='namo950815@gmail.com',
+    description='Sogang-ISDS setup'
+)
